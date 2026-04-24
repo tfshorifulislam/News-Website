765cc6aad8676a9075921e64c58ea956996d9034
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-25 00:12:16 +0600

    navbar done

--- src/components/latestNews/LatestNews.jsx
+++ src/components/latestNews/LatestNews.jsx
@@ -7,8 +7,11 @@ const LatestNews = () => {
             <button className='bg-[#D72050] font-medium text-xl leading-8 py-2 px-6 cursor-pointer text-white'>
                 Latest
             </button>
-            <Marquee>
-                Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
+            <Marquee
+            pauseOnHover={true}
+            speed={80}
+            className='font-semibold leading-8 text-lg text-[#403F3F]'>
+                Match Highlights: Germany vs Spain — as it happened! Germany and Spain delivered a thrilling encounter filled with intensity.
             </Marquee>
         </div>
     );
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-
+import avatar from '@/assets/user.png'
 const Navbar = () => {
     return (
-        <div>
-            <h1>
-                This is Navbar
-            </h1>
+        <div className='flex justify-between mt-5 items-center'>
+            <div></div>
+            <ul className='flex justify-center gap-3 text-[#706F6F] text-lg leading-8'>
+                <li>
+                    <Link href={'/'}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href={'/about'}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href={'/career'}>
+                        Career
+                    </Link>
+                </li>
+            </ul>
+            <div className='flex items-center justify-center gap-2'>
+                <Image src={avatar} alt='user Avatar' width={41} height={41} />
+                <button className='px-10 py-2  bg-[#403F3F] text-white font-semibold text-xl'>
+                    <Link href={'/login'}>
+                        Login
+                    </Link>
+                </button>
+            </div>
         </div>
     );
 };
