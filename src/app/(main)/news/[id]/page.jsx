284e7c48c7725fb49321901dc2b8a41d6efae640
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-24 19:28:24 +0600

    project or routing setup

---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            News Details Page
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/shared/Header";
+import Navbar from "@/components/shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body
+        className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
